4ba8958f0ad125ae844b149a9904871332d48218
Lutfia Husna Khoirunnisa <99700225+lutfiahusnakhoirunnisa@users.noreply.github.com>
2022-07-24 18:47:36 +0700

    Add files via upload

---
+++ risk_rating.R
@@ -0,0 +1,89 @@
+#library importing
+library(e1071)
+library(caret)
+library(dplyr)
+
+#data importing
+df<-credit_datas
+dim(df)
+str(df)
+summary(df)
+
+#data cleaning
+index<-c("pendapatan_setahun_juta", "durasi_pinjaman_bulan", "jumlah_tanggungan", "risk_rating")
+datarisk<-df[index]
+datarisk$risk_rating<-as.factor(datarisk$risk_rating)
+str(datarisk)
+head(datarisk)
+
+#datasplitting
+set.seed(123)
+datasample<-sample(1:nrow(datarisk), size = 0.8*nrow(datarisk)) #80%data digunakan sbg data train
+datatrain<-datarisk[datasample,]
+datatest<-datarisk[-datasample,]
+dim(datatrain)
+dim(datatest)
+
+#classification modeling
+##using naive bayes method
+modelnaive<-naiveBayes(x=datatrain %>% select(-risk_rating), y=datatrain$risk_rating)
+
+###testing the model
+predictnaive<-predict(modelnaive, datatest, type="class")
+tbpredictnaive <- data.frame(datatest,risk_naive)
+head(tbpredictnaive)
+
+###evaluating prediction
+naiveeval<- confusionMatrix(data=risk_naive, reference=datatest$risk_rating)
+naiveeval
+
+##using support vector machine
+####classification modeling
+modelsvm<-svm(risk_rating~.-risk_rating,datatrain, gamma=0.5, type="C-classification")
+
+###testing the model
+predictsvm<-predict(modelsvm, datatest, type="class")
+tbpredictsvm <- data.frame(datatest,predictsvm)
+head(tbpredictsvm)
+
+###evaluating prediction
+svmeval<- confusionMatrix(data=predictsvm, reference=datatest$risk_rating)
+svmeval
+
+##using random forest
+controlrf <- trainControl (method="repeatedcv", number=5, repeats=2) #k-fold cross validation
+modelrf <- train(risk_rating ~., data=datatrain, method="rf", trControl=controlrf)
+
+###testing the model
+predictrf <- predict(modelrf, newdata = datatest, type = "raw")
+tbpredictrf <- data.frame(datatest,predictrf)
+head(tbpredictrf)
+
+###evaluating prediction
+rfeval<- confusionMatrix(data=predictrf, reference=datatest$risk_rating)
+rfeval
+
+##using k-nearest neighbor
+###classification modeling
+library(caret)
+controlknn <- trainControl (method="repeatedcv", number=5, repeats=2) 
+modelknn<-train(risk_rating ~ ., data = datatrain, method = "knn", trControl = controlknn, preProcess = c("center","scale"), tuneLength = 20)
+
+##testing the model
+predictknn <- predict(modelknn, newdata = datatest, type = "raw")
+tbpredictrf <- data.frame(datatest,predictknn)
+head(tbpredictrf)
+
+###evaluating the model
+confusionMatrix(data = predictknn, reference=datatest$risk_rating)
+
+##using decision tree
+###classification modeling
+library(rpart)
+modeldt <-rpart(risk_rating~., data=datatrain, method="class")
+
+##testing the model
+predictdt <- predict(modeldt, newdata = datatest, type = "class")
+
+###evaluating the model
+confusionMatrix(data = predictdt, reference=datatest$risk_rating)
